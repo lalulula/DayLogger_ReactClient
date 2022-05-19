33a6e9331369c7c63a4521ed732beae0a58f1bb2
hethegrace0310 <ha-eun.park@stonybrook.edu>
2022-05-20 00:49:03 +0900

    cors error handling6

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,10 +1,10 @@
 const backendURL = "https://cse316final.herokuapp.com";
 const defaultHeaders = {
   headers: {
-    "Content-Type": "application/json; charset=UTF-8",
-    // credentials: "include",
-    withCredentials: true,
-    "Access-Control-Allow-Origin": backendURL,
+    "Content-Type": "application/json",
+    credentials: "include",
+    // withCredentials: true,
+    // "Access-Control-Allow-Origin": backendURL,
   },
 };
 
