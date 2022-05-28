d98d5b474e575d2be22629089d9a7e727b162aa9
Yunah Kim <yunah.kim@stonybrook.edu>
2022-05-28 17:42:29 +0900

    removed comments2

--- src/components/LogDay.js
+++ src/components/LogDay.js
@@ -3,10 +3,12 @@ import LogDayQuestions from "./LogDayQuestions";
 import { getQuestionAPI, updateQuestionAPI } from "../api/questionAPI";
 
 function LogDay({handleSubmit, questions, setQuestions, user }) {
-  useEffect(() => {
-    console.log("RELOADING QUESTIONS");
-    console.log("QUESTIONS:", questions);
-  }, [questions]);
+
+  // //NOTE made for testing purposes
+  // useEffect(() => {
+  //   console.log("RELOADING QUESTIONS");
+  //   console.log("QUESTIONS:", questions);
+  // }, [questions]);
 
   useEffect(() => {
     function fetchData() {
